e6eb5d37280163cf66c6b3be7b3a5ec1cf9b3437
Timon Rimann <timon.rimann@zhdk.ch>
2024-11-26 13:51:05 +0100

    update proj 4

--- projects/01MainAssignments/project4/script.js
+++ projects/01MainAssignments/project4/script.js
@@ -1,4 +1,7 @@
 //Unconventional/unconvenient Micro-interaction
+//let currentTextSize;
+//let currentTextColor;
+//let currentBackground;
 
 function preload() {
 	font = loadFont('/Fonts/S_S_Metroviation_Five.otf');
@@ -15,37 +18,58 @@ function setup() {
 }
 
 function draw() {
-	//let r = (mouseY / 3)
+	let r = (mouseY / 3)
 	//let g = (mouseY / 4)
 	//let b = (mouseY)
 
+	//stored values
+	//textSize(currentTextSize);
+	//fill(currentTextColor);
+
+
 	//background effect
 	if (mouseX > 80 && mouseX < 140) {
-		background(mouseY / 2, mouseY / 3, 255);
+		let currentBackground = (mouseY / 2, mouseY / 3, 255);
+		background(mouseY / 2, mouseY / 3, 255)
+		//background(currentBackground);
+		//currentBackground = (mouseY / 2, mouseY / 3, 255);
+
 	}
 
 	//Textsize effect
 	if (mouseX > 150 && mouseX < 210) {
 		strokeWeight(6)
-		//fill(r)
+		fill(r)
 		textSize(constrain(mouseY, 110, height - 110) / 2)
 	}
 
-	//Textcolor effect
+	push()
+	//shifthing effect
 	if (mouseX > 220 && mouseX < 280) {
+		fill(255)
+		stroke(0)
+		translate(0, constrain(mouseY, 110, height - 110) / 5 - 100)
+	}
+
+	//Textcolor effect
+	if (mouseX > 290 && mouseX < 350) {
+		fill(255)
 		fill(constrain(mouseY, 110, height - 110) / 2, constrain(mouseY, 110, height - 110) / 6, constrain(mouseY, 110, height - 110) / 3)
 		//fill(r, g, b)
 	}
 
 
-	//textfield
-	text('Text', width / 2, height / 2);
 
+
+	//textfield
+	text('text', width / 2, height / 2);
+	pop()
 
 	//show sliders
 	slider(100)
 	slider(170)
 	slider(240)
+	slider(310)
 
 
 }
